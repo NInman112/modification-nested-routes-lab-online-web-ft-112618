25c02c10327170408d6219de2eaeb37fb7cb8069
Nick Inman <nick@theoatmeal.com>
2019-02-18 17:34:56 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -46,7 +46,7 @@ class SongsController < ApplicationController
     if params[:artist_id]
       artist = Artist.find_by(id: params[:artist_id])
       if artist.nil?
-        redirect_to artist_path
+        redirect_to artists_path
       else
         @song = artist.post.find_by(id: params[:id])
         redirect_to artist_songs_path(artist)
