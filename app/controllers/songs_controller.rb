3fad9aff06405831a775b79743616db8fa65ef61
Nick Inman <nick@theoatmeal.com>
2019-02-18 17:43:52 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -18,7 +18,7 @@ class SongsController < ApplicationController
       @song = @artist.songs.find_by(id: params[:id])
       if @song.nil?
         flash[:alert] = "Song not found"
-        redirect_to artist_songs_path(@artist)
+        redirect_to artist_songs_path
       end
     else
       @song = Song.find(params[:id])
